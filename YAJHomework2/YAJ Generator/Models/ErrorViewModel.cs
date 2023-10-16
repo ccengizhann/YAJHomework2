c33d9d85a73172fe04cea24c968ee537779ab620
Cengizhan <cengizhancivelek@gmail.com>
2023-10-16 03:33:59 +0300

    initial commit

---
+++ YAJHomework2/YAJ Generator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YAJ_Generator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ YAJHomework2/YAJ Generator/Models/HomeIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace YAJ_Generator.Models
+{
+    public class HomeIndexViewModel
+    {
+        public string Prompt { get; set; }
+        public int ImageCount { get; set; }
+
+        public List<string> ImageUrls { get; set; }
+
+        public HomeIndexViewModel()
+        {
+            ImageUrls = new List<string>();
+        }
+    }
+}
